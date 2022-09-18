cca5b583cf24e357a5137d0cc2582c57cadea89e
Nursultan <satibaev.nyrsyltan@gmail.com>
2022-09-18 21:22:32 +0600

    Uber

--- script.js
+++ script.js
@@ -3,5 +3,8 @@
 // const result=confirm("Are you here?");
 // console.log(result);
 
-const answer = prompt("Вам есть 18?", "18");
-console.log(typeof(answer));
+if (4 == 4) {
+	console.log("OK!");
+} else {
+	console.log("Error!");
+}
